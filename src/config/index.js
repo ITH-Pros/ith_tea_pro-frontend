2c6c4adf0067f6ad3f907618560337a8576ec934
Aditya kumar <aditya@ith.tech>
2023-03-14 11:36:03 +0530

    integrate on change funtinality in rating

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,9 @@
 
 
 // let url = 'http://192.168.1.38:9000'
-// let url = 'http://192.168.29.78:9000'
+let url = 'http://192.168.29.240:9000'
 // let url = 'http://localhost:9000'
 // let url = 'http://192.168.29.131:9000';
-let url = "https://projectapi.ith.tech";
+// let url = "https://projectapi.ith.tech";
 
 exports.baseURL = url
