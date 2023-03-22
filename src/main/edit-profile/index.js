a294943035a17a47ee4cb3768b7441e4c231a8ab
yash jangid <yash@ith.tech>
2023-03-22 12:24:17 +0530

    profile upload functionality

--- src/main/edit-profile/index.js
+++ src/main/edit-profile/index.js
@@ -4,6 +4,7 @@ import Loader from '../../components/Loader';
 import Toaster from '../../components/Toaster';
 import { editLogedInUserDetails, getLogedInUserDetails } from '../../services/user/api';
 import './index.css'
+import ImageUpload from "./imageUpload";
 
 function UserForm(props) {
 
@@ -11,8 +12,8 @@ function UserForm(props) {
 	
   const [name, setName] = useState('');
   const [loading, setLoading] = useState(false);
-  const [toasterMessage, setToasterMessage] = useState("");
   const [toaster, showToaster] = useState(false);
+  const [toasterMessage, setToasterMessage] = useState("");
   const [role, setRole] = useState(''); // ['SUPER_ADMIN', 'ADMIN', 'CONTRIBUTOR']
   const [email, setEmail] = useState('');
   const [employeeId, setEmployeeId] = useState('');
@@ -149,136 +150,140 @@ function UserForm(props) {
   }
 
   return (
-	<div className="addUserFrom"> 
-    <form className='row'>
-      <div className="form-group col-4">
-        <label htmlFor="name">Name:</label>
-        <input
-          type="text"
-          id="name"
-          value={name}
-          onChange={(event) => setName(event.target.value)}
-		  readOnly={!isEditable}
-        />
-      </div>
-	  <div className="form-group col-4">
-		<label htmlFor="role">Role:</label>
-		<input 
-			type="text"
-			id="role"
-			value={role}
-			onChange={(event) => setRole(event.target.value)}
-			readOnly={true}
-		/>
-	  </div>
-     
-      <div className="form-group col-4">
-        <label htmlFor="employeeId">Employee ID:</label>
-        <input
-          type="text"
-          id="employeeId"
-          value={employeeId}
-          onChange={(event) => setEmployeeId(event.target.value)}
-		  readOnly={!isEditable}
-        />
-      </div>
-     
-      <div className="form-group col-4">
-        <label htmlFor="dob">Date of Birth:</label>
-        <input
-          type="date"
-          id="dob"
-          value={formatDate(dob)}
-		  max={formatDate(new Date())}
-          onChange={(event) => setDob(event.target.value)}
-		  readOnly={!isEditable}
-        />
-      </div>
-      <div className="form-group col-4">
-        <label htmlFor="designation">Designation:</label>
-        <input
-          type="text"
-          id="designation"
-          value={designation}
-          onChange={(event) => setDesignation(event.target.value)}
-		  readOnly={!isEditable}
-        />
-      </div>
-      <div className="form-group col-4">
-        <label htmlFor="department">Department:</label>
-        <input
-          type="text"
-          id="department"
-          value={department}
-          onChange={(event) => setDepartment(event.target.value)}
-		  readOnly={!isEditable}
-        />
-      </div>
-     
+    <div className="addUserFrom">
+      <form className="row">
+        <div className="form-group col-4">
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+            readOnly={!isEditable}
+          />
+        </div>
+        <div className="form-group col-4">
+          <label htmlFor="role">Role:</label>
+          <input
+            type="text"
+            id="role"
+            value={role}
+            onChange={(event) => setRole(event.target.value)}
+            readOnly={true}
+          />
+        </div>
+
+        <div className="form-group col-4">
+          <label htmlFor="employeeId">Employee ID:</label>
+          <input
+            type="text"
+            id="employeeId"
+            value={employeeId}
+            onChange={(event) => setEmployeeId(event.target.value)}
+            readOnly={!isEditable}
+          />
+        </div>
+
+        <div className="form-group col-4">
+          <label htmlFor="dob">Date of Birth:</label>
+          <input
+            type="date"
+            id="dob"
+            value={formatDate(dob)}
+            max={formatDate(new Date())}
+            onChange={(event) => setDob(event.target.value)}
+            readOnly={!isEditable}
+          />
+        </div>
+        <div className="form-group col-4">
+          <label htmlFor="designation">Designation:</label>
+          <input
+            type="text"
+            id="designation"
+            value={designation}
+            onChange={(event) => setDesignation(event.target.value)}
+            readOnly={!isEditable}
+          />
+        </div>
+        <div className="form-group col-4">
+          <label htmlFor="department">Department:</label>
+          <input
+            type="text"
+            id="department"
+            value={department}
+            onChange={(event) => setDepartment(event.target.value)}
+            readOnly={!isEditable}
+          />
+        </div>
       </form>
-      <form className='row'>
-      <div className="form-group">
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-		  readOnly={true}
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="github">Github:</label>
-        <input
-          type="url"
-          id="github"
-          value={github}
-          onChange={(event) => setGithub(event.target.value)}
-		  readOnly={!isEditable}
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="twitter">Twitter:</label>
-        <input
-          type="url"
-          id="twitter"
-          value={twitter}
-          onChange={(event) => setTwitter(event.target.value)}
-		  readOnly={!isEditable}
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="linkedin">LinkedIn:</label>
-        <input
-          type="url"
-          id="linkedin"
-          value={linkedin}
-          onChange={(event) => setLinkedin(event.target.value)}
-		  readOnly={!isEditable}
-        />
-      </div>
-	  {isEditable && (
-		<button onClick={handleSubmit} className="submit-button"> Update
-  </button>)}
- 
-  {!isEditable && (
-	<button className="submit-button edit" onClick={handleEditClick} >Edit
-  </button>)
-	}
+      <form className="row">
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            readOnly={true}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="github">Github:</label>
+          <input
+            type="url"
+            id="github"
+            value={github}
+            onChange={(event) => setGithub(event.target.value)}
+            readOnly={!isEditable}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="twitter">Twitter:</label>
+          <input
+            type="url"
+            id="twitter"
+            value={twitter}
+            onChange={(event) => setTwitter(event.target.value)}
+            readOnly={!isEditable}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="linkedin">LinkedIn:</label>
+          <input
+            type="url"
+            id="linkedin"
+            value={linkedin}
+            onChange={(event) => setLinkedin(event.target.value)}
+            readOnly={!isEditable}
+          />
+        </div>
+        <div>
+          <h5>Profile Image</h5>
+          <ImageUpload />
+        </div>
+        {isEditable && (
+          <button onClick={handleSubmit} className="submit-button">
+            {" "}
+            Update
+          </button>
+        )}
 
- 
-</form>
+        {!isEditable && (
+          <button className="submit-button edit" onClick={handleEditClick}>
+            Edit
+          </button>
+        )}
+      </form>
 
-{toaster && (
-					<Toaster
-						message={toasterMessage}
-						show={toaster}
-						close={() => showToaster(false)} />
-				)}
-				{loading ? <Loader /> : null}
-
-</div>
-
+      {toaster && (
+        <Toaster
+          message={toasterMessage}
+          show={toaster}
+          close={() => showToaster(false)}
+        />
+      )}
+      {loading ? <Loader /> : null}
+    </div>
   );
 }
 
