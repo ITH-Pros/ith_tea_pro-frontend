7bc17d58af1b475c9e689fe22d706bb60c8dcc43
Aditya kumar <aditya@ith.tech>
2023-06-16 11:13:48 +0530

    code merge

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,20 +1,5 @@
-// import React from "react";
+
 import './Loader.css';
-// const Loader = () => {
-//     return (
-//         <React.Fragment>
-//             <div className="loader-wrapper">
-//                 <div className="loader-circle"></div>
-//                 <div className="loader-circle"></div>
-//                 <div className="loader-circle"></div>
-//                 <div className="loader-shadow"></div>
-//                 <div className="loader-shadow"></div>
-//                 <div className="loader-shadow"></div>
-//             </div>
-//         </React.Fragment>
-//     )
-// }
-// export default Loader;
 import React from 'react';
 import styled, { keyframes } from 'styled-components';
 
