4043bc6fa1ea11afc1057710eda39e251036c875
vijayith <vijay@ith.tech>
2023-09-08 13:17:56 +0530

    dev tool removed

--- src/main.jsx
+++ src/main.jsx
@@ -21,7 +21,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
           <App />
         </AuthProvider>
       </BrowserRouter>
-      <ReactQueryDevtools />
+      {/* <ReactQueryDevtools /> */}
     </QueryClientProvider>
   </React.StrictMode>
 );
