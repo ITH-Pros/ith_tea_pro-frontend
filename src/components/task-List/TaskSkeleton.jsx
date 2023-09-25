1316bc92fdff54c113788744de923cfe4c6c0c72
Aditya kumar <aditya@ith.tech>
2023-09-25 12:15:37 +0530

    fix the issue #251

--- src/components/task-List/TaskSkeleton.jsx
+++ src/components/task-List/TaskSkeleton.jsx
@@ -18,26 +18,34 @@ import "../../pages/Tasks/tasks.css";
 const TaskSkeleton = () => {
   return (
     <SkeletonTheme color="#f3f3f3" highlightColor="#ecebeb">
-    {Array.from({ length: 6 }).map((_, index) => (
-      <Accordion.Item >
-   
-          <Accordion.Header>
-            <Skeleton style={{marginRight:'5px'}} height={25} width={200} /> /{" "}
-            <Skeleton style={{marginLeft:'5px'}} height={25} width={200} />
-          </Accordion.Header>
+      {Array.from({ length: 15 }).map((_, index) => (
+        <Accordion.Item>
+          <div style={{ display: "flex", alignItems: "center" , marginLeft:'30px' }}>
+            <Skeleton style={{ marginRight: "5px" }} height={25} width={200} />
+            <Skeleton style={{ marginLeft: "5px" }} height={25} width={200} />
+          </div>
 
-        <div className="d-flex rightTags pt-2">
-
+          <div className="d-flex rightTags pt-2">
             <div>
-                <Skeleton style={{ marginTop:'15px' }} borderRadius={5} height={20} width={300} />
+              <Skeleton
+                style={{ marginTop: "15px" }}
+                borderRadius={5}
+                height={20}
+                width={300}
+              />
             </div>
 
-          <div style={{ position: "absolute", right: "10px", top: "10px" }}>
-          <Skeleton style={{ marginTop:'15px' }} borderRadius={5} height={20} width={20} />
+            <div style={{ position: "absolute", right: "10px", top: "10px" }}>
+              <Skeleton
+                style={{ marginTop: "15px" }}
+                borderRadius={5}
+                height={20}
+                width={20}
+              />
+            </div>
           </div>
-        </div>
-      </Accordion.Item>
-    ))}
+        </Accordion.Item>
+      ))}
     </SkeletonTheme>
   );
 };
